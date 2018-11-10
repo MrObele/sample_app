852c8d8ee465b9c7a132634a422bf42e0ce47025
mrobele <obeledavid26@gmail.com>
2018-11-10 16:46:48 +0100

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,14 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://dave-sample-app.herokuapp.com'
+  host = 'dave-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app113406105@heroku.com'],
-    :password       => ENV['dclngjcs7413'],
+    :user_name      => ENV['app115015763@heroku.com'],
+    :password       => ENV['nhtfvwuc5076'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
