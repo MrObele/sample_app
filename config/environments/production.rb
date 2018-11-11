515810e6e72324ac186dae268a9e55ac5e0c7993
mrobele <obeledavid26@gmail.com>
2018-11-11 02:41:06 +0100

    secured sendgrid environment viriables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app115015763@heroku.com'],
-    :password       => ENV['nhtfvwuc5076'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
